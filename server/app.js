419278e394d150321004cc82e447a6e9c917ba1d
Md. Shahriar Anwar <anwarshahriar@localhost.localdomain>
2017-11-14 01:40:26 +0600

    Added simple local ip find implementation

--- server/app.js
+++ server/app.js
@@ -1,1 +1,8 @@
-console.log('Hello World')
+const localIP = require('my-local-ip')
+
+const ip = localIP()
+if (ip) {
+    console.log(localIP())
+} else {
+    console.log("Please, connect to a wifi network and retry")
+}
