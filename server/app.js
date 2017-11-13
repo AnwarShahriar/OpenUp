331d2fceb57e295ce24bfcb59530f92731d7806e
Md. Shahriar Anwar <anwarshahriar@localhost.localdomain>
2017-11-14 02:01:40 +0600

    Added api to consume url from client

--- server/app.js
+++ server/app.js
@@ -1,8 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
 const localIP = require('my-local-ip')
+const app = express()
 
-const ip = localIP()
-if (ip) {
-    console.log(localIP())
-} else {
-    console.log("Please, connect to a wifi network and retry")
+app.use(bodyParser.json())
+
+app.post('/openup', (req, res) => {
+    let msg;
+    if (req.body.url) {
+        msg = 'Will try to open up url with default browser'
+    } else {
+        msg = 'Please, give me a url'
+    }
+    
+    console.log(msg)
+    res.status(200).json({ msg })
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`)
+    showLocalIP()
+})
+
+const showLocalIP = () => {
+    const ip = localIP()
+    if (ip) {
+        console.log(`Your local ip is: ${ip}`)
+    } else {
+        console.log("Please, connect to a wifi network and retry")
+    }
 }
